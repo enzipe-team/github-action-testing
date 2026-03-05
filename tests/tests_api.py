43be7ac60dcdd2cbb718592c16236894d524ef13
Muaz Ahmed <python.team.enzipe@gmail.com>
2026-03-05 15:17:11 +0500

    fix test file naming for pytest

--- tests/tests_api.py
+++ tests/tests_api.py
@@ -1,6 +1,6 @@
 from app import app
 
-def test_home():
+def test_home():  # ✅ must start with test_
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
