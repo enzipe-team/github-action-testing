0804546ed008ebb88d1df67c6ff9d93acd05aa5f
Muaz Ahmed <python.team.enzipe@gmail.com>
2026-03-05 15:21:38 +0500

    fix pytest import with PYTHONPATH

--- tests/tests_api.py
+++ tests/tests_api.py
@@ -1,6 +1,7 @@
-from ..app import app
+from app import app  # import Flask app
 
-def test_home():  # ✅ must start with test_
+def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
+    assert response.json == {"message": "API running"}
