8fe309f6096f1b570899cd974289288da3db5c49
Muaz Ahmed <python.team.enzipe@gmail.com>
2026-03-05 15:32:24 +0500

    fix pytest import with PYTHONPATH

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "API is running"})
+    return jsonify({"message": "API is running again"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
